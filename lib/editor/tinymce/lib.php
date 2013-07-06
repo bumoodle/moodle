7826cc6eebd9d2bf2d07f432f87ee081e36ab6d2
Kyle Temkin <ktemkin@binghamton.edu>
2013-07-06 18:30:11 -0400

    BU-101 add initial CFG setting to control whether editors should collapse

--- lib/editor/tinymce/lib.php
+++ lib/editor/tinymce/lib.php
@@ -106,7 +106,11 @@ class tinymce_texteditor extends texteditor {
         if ($fpoptions) {
             $PAGE->requires->js_init_call('M.editor_tinymce.init_filepicker', array($elementid, $fpoptions), true);
         }
-        $this->initialise_collapse_js();
+
+        //If automatic collapsing of editors is on, allow the editors to be collapsed. 
+        if(!property_exists($CFG, 'collapse_editors') || $CFG->collapse_editors) {
+            $this->initialise_collapse_js();
+        }
     }
 
     protected function get_init_params($elementid, array $options=null) {
